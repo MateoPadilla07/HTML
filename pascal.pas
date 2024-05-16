d32a19d444584239fcf79842ff31da5444b4a993
Mateo Padilla <168492953+MateoPadilla07@users.noreply.github.com>
2024-05-16 19:26:35 -0300

    Add files via upload

---
+++ pascal.pas
@@ -0,0 +1,11 @@
+program dadada;
+var num1, num2, num integer
+begin
+    writeln('Escriba el primer numero');
+    readln(num1);
+    writeln('Escribs el segundo numero');
+    readln(num2);
+    num:= num1 + num2
+    writeln('El suma de', num1 'y', num2 'es: ', num);
+    
+end;
