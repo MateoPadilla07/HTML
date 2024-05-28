dc07462c377d607b679520ba25e0b7a4bcc28bd9
Mateo Padilla <168492953+MateoPadilla07@users.noreply.github.com>
2024-05-27 21:42:17 -0300

    Add files via upload

---
+++ program hola;.pas
@@ -0,0 +1,15 @@
+program hola;
+var
+num1, num2, total; integer
+begin
+write ('Ingrese el primer numero');
+read (num1);
+write ('ingrese el segundo numero');
+read (num2);
+Total := num1 + num2;
+if num1 := num2;
+ write ('el resultado es :'), total;
+ else num1 <> num2
+ write ('son distintos');
+    
+end.
